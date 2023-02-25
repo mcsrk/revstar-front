7ca31136e690dd28a4faa7ecc63f45a690f94f95
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-02-25 15:30:38 -0500

    fixed style issues with tailwind + antd classes

--- src/components/common/TableTitle.js
+++ src/components/common/TableTitle.js
@@ -2,10 +2,10 @@ import { Button, Row } from "antd";
 
 const TableTitle = ({ title, btnTitle, btnAction, icon = null }) => {
 	return (
-		<Row justify="space-between">
+		<Row justify="space-between" align={"middle"}>
 			<h1 className="text-xl  sm:text-2xl font-bold">{title}</h1>
 			{btnTitle && btnAction && (
-				<Button onClick={btnAction} icon={icon}>
+				<Button type="primary" onClick={btnAction} icon={icon}>
 					{btnTitle}
 				</Button>
 			)}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {
-      fontFamily: {
-        poppins: ["Poppins", "sans-serif"],
-      },
-    },
-  },
-  plugins: [],
+	corePlugins: {
+		preflight: false,
+	},
+	content: ["./src/**/*.{js,jsx,ts,tsx}"],
+	theme: {
+		extend: {
+			fontFamily: {
+				poppins: ["Poppins", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
 };
