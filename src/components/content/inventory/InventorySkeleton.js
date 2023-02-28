98fa71dd4cf5f65eda58eb785ef163e06a93350c
mcsrk <49255542+mcsrk@users.noreply.github.com>
2023-02-28 10:15:20 -0500

    Improved inventory skeleton styles

--- src/components/content/inventory/InventorySkeleton.js
+++ src/components/content/inventory/InventorySkeleton.js
@@ -1,16 +1,20 @@
-import { Skeleton } from "antd";
-import React from "react";
+import { Row, Skeleton, Space } from "antd";
 
 const InventorySkeleton = () => {
 	return (
-		<>
-			<Skeleton.Button active />
-			<br />
-			<br />
-			<Skeleton.Input active />
-			<br />
-			<Skeleton active />
-		</>
+		<Space direction="vertical" className="w-full">
+			<Row justify="space-between">
+				<Skeleton.Button active />
+				<Skeleton.Button active />
+			</Row>
+
+			<Row justify="space-between">
+				<Skeleton.Input active />
+				<Skeleton.Input active />
+			</Row>
+
+			<Skeleton active className="mt-2" />
+		</Space>
 	);
 };
 
